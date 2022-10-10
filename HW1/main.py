ff896f578159906f4b3a49a7fc5c956dfad9cc83
Chilin <chilin.cs07@nycu.edu.tw>
2022-10-10 13:45:42 +0800

    Add CNN with LSTM, acc: 0.36

--- HW1/main.py
+++ HW1/main.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import random
 
 
 def most_common(lst):
@@ -14,70 +15,119 @@ def most_common(lst):
     return lst.most_common(1)[0][0]
 
 
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
+
 def read_mp4(mp4_filename: str)-> List[np.ndarray]:
     cap = cv2.VideoCapture(str(mp4_filename))
     frames = list()
+    frame_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    # if frame length < SEQ_LENGTH, padding by repeat image
+    more_40_flag = frame_length>40
+    skip_num = int(frame_length/40)-1
+
+    if skip_num>=0:
+        skip_mod = frame_length%40
+        if skip_mod<=0:
+            skip_cnt = skip_num
+        else:
+            skip_cnt = skip_num + 1
+            skip_mod -= 1
+    else: 
+        more_40_flag = False
+        
     while cap.isOpened():
         ret, frame = cap.read()
-        
+
         # End of video or error occured
         if not ret:
             break
 
+        if more_40_flag:
+            if skip_cnt>0:
+                skip_cnt -= 1
+                continue
+        
+        if more_40_flag:
+            if skip_mod<=0:
+                skip_cnt = skip_num
+            else:
+                skip_cnt = skip_num + 1
+                skip_mod -= 1            
+
         # print(ret, frame/255.0, frame.shape, type(frame))
-        frame = cv2.resize(frame, (120, 90), interpolation=cv2.INTER_AREA)
+        frame = crop_center_square(frame)
+        frame = cv2.resize(frame, (224, 224))
+        frame = frame[:, :, [2, 1, 0]]
+
         frames.append(frame/255.0)
 
     cap.release()
     cv2.destroyAllWindows()
 
-    return np.array(frames)
+    # if number of frame less then require seq length
+    # repeating last image
+    last_frame = frames[-1]
+    while len(frames)<40:
+        frames.append(last_frame)
+
+    frames = np.array(frames)
+    return frames
 
 
-def get_mp4_frames(mp4_filename: str)-> List[np.ndarray]:
+def get_mp4_frames(mp4_filename: str)-> int:
+    """
+    Get total number frame of mp4
+    """
     cap = cv2.VideoCapture(str(mp4_filename))
-    cnt = 0 
+    frame_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # End of video or error occured
-        if not ret:
-            break
-
-        cnt +=1 
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return cnt
+    return frame_length
 
 
-def data_generator(filename_list: List[str]):
+def data_generator(filename_list: List[str], batch_size=1):
     """Generator for read mp4 filename from training or testing folder,
     would yield each frame data in video and ground true label(class)
     """
     idx = 0
     while idx<len(filename_list):
-        mp4_filename, ground_true = filename_list[idx]
-        mp4_filename = mp4_filename.decode('ascii')
-        pixel_data = read_mp4(mp4_filename)
-        yield (pixel_data, [ground_true]*len(pixel_data))
-        idx += 1
+        pixel_data_list = list()
+        ground_true_list = list()
+        for j in range(batch_size):
+            if idx>=len(filename_list):
+                break
+            mp4_filename, ground_true = filename_list[idx]
+            mp4_filename = mp4_filename.decode('ascii')
+            pixel_data = read_mp4(mp4_filename)
+            pixel_data_list.append(pixel_data)
+            ground_true_list.append(int(ground_true))
+            idx += 1
+        yield (pixel_data_list, ground_true_list)
 
 
-def testdata_generator(filename_list: List[str]):
+def testdata_generator(filename_list: List[str], batch_size=1):
     """Generator for read mp4 filename from training or testing folder,
     would yield each frame data in video and ground true label(class)
     """
     idx = 0
     while idx<len(filename_list):
-        mp4_filename = filename_list[idx]
-        mp4_filename = mp4_filename.decode('ascii')
-        pixel_data = read_mp4(mp4_filename)
-        yield pixel_data
-        idx += 1
+        pixel_data_list = list()
+        for j in range(batch_size):
+            if idx>=len(filename_list):
+                break
+            mp4_filename = filename_list[idx]
+            mp4_filename = mp4_filename.decode('ascii')
+            pixel_data = read_mp4(mp4_filename)
+            pixel_data_list.append(pixel_data)
+            idx += 1
+        yield pixel_data_list
+
 
 
 train_dir_path = "/home/chilin/NYCU-Cloud-Computing-and-Big-Data-Analytics/HW1/train"
@@ -93,14 +143,31 @@ test_files = list()
 # all information would be a list of tuple(mp4 filepath, class)
 
 test_files = [os.path.join(test_dir_path, i) for i in os.listdir(test_dir_path)]
-
+# test_files = [test_files[0], test_files[1]]
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(90, 120, 3)))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(39, activation='softmax'))
+
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(16, (3, 3), padding='same',activation = 'relu'),
+                            input_shape = (40, 224, 224, 3)))
+
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D((4, 4)))) 
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(0.25)))
+
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation = 'relu')))
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D((4, 4))))
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(0.25)))
+
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64, (3, 3), padding='same',activation = 'relu')))
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D((2, 2))))
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(0.25)))
+
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64, (3, 3), padding='same',activation = 'relu')))
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D((2, 2))))
+#model.add(TimeDistributed(Dropout(0.25)))
+                                    
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())) 
+model.add(tf.keras.layers.LSTM(100))
+model.add(tf.keras.layers.Dense(39, activation = 'softmax'))
 
 print(model.summary())
 
@@ -121,26 +188,30 @@ print(len(train_files))
 print(len(valid_files))
 print(train_files[0])
 
+# shuffle
+random.shuffle(train_files)
+random.shuffle(valid_files)
+
+# generate Dataset
 train_dataset = tf.data.Dataset.from_generator(
     data_generator,
     args=(train_files,),
     output_types=(tf.float32, tf.float32),
-    output_shapes=([None, 90, 120, 3], [None])
+    output_shapes=([None, 40, 224, 224, 3], [None])
 )
 
 valid_dataset = tf.data.Dataset.from_generator(
     data_generator,
     args=(valid_files,),
     output_types=(tf.float32, tf.float32),
-    output_shapes=([None, 90, 120, 3], [None])
+    output_shapes=([None, 40, 224, 224, 3], [None])
 )
 
-print(test_files[0])
 test_dataset = tf.data.Dataset.from_generator(
     testdata_generator,
     args=(test_files,),
     output_types=(tf.float32),
-    output_shapes=([None, 90, 120, 3])
+    output_shapes=([None, 40, 224, 224, 3])
 )
 
 opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
@@ -151,12 +222,12 @@ model.compile(
 )
 
 # history = model.fit(
-#     train_dataset, 
+#     train_dataset,
 #     validation_data=valid_dataset,
-#     epochs=5
+#     epochs=30,
 # )
-# model.save(f"CNN_10_09")
-model = tf.keras.models.load_model(f"CNN_10_09")
+# model.save(f"CNN_10_09_2")
+model = tf.keras.models.load_model(f"CNN_10_09_2")
 
 # plt.plot(history.history['accuracy'])
 # plt.title('c accuracy')
@@ -177,18 +248,19 @@ model = tf.keras.models.load_model(f"CNN_10_09")
 result = model.predict(
     test_dataset
 )
-
 tmp_y_pred = list()
-for i in result:
-    tmp_y_pred.append(np.argmax(i))
-
 y_pred = list()
-cur = 0
-for test_file in test_files:
-    test_file = test_file.split("/")[-1]
-    frame_cnt = get_mp4_frames(test_file)
-    y_pred.append(most_common(tmp_y_pred[cur: cur+frame_cnt-1]))
-    cur += frame_cnt
+
+for i in result:
+    # print(np.argmax(i))
+    y_pred.append(np.argmax(i))
+
+# cur = 0
+# for test_file in test_files:
+#     test_file = test_file.split("/")[-1]
+#     frame_cnt = get_mp4_frames(test_file)
+#     y_pred.append(most_common(tmp_y_pred[cur: cur+frame_cnt-1]))
+#     cur += frame_cnt
 
 print(len(test_files), len(y_pred))
      
@@ -196,4 +268,4 @@ df = pd.DataFrame({
     "name": [i.split("/")[-1] for i in test_files],
     "label": y_pred
 })
-df.to_csv("result.csv")
+df.to_csv("result_1009_tmp.csv")
